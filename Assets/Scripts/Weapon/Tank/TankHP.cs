fa5ba52146d23cf2b2dd1034ed24a8949b2a878a
whylosss <illiapalamarchuk.0@gmail.com>
2024-06-27 18:56:09 +0200

    asd

--- Assets/Scripts/Weapon/Tank/TankHP.cs
+++ Assets/Scripts/Weapon/Tank/TankHP.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class TankHP : MonoBehaviour, ITankHealth
 {
+    [SerializeField] private ParticleSystem _effect;
+    [SerializeField] private Transform _pos;
+
     private EventBus _eventBus;
     private Rigidbody _rb;
+
     private void Start()
     {
         _eventBus = ServiceLocator.Current.Get<EventBus>();
@@ -14,6 +18,7 @@ public class TankHP : MonoBehaviour, ITankHealth
     }
     public void Destroy()
     {
+        Instantiate(_effect, _pos.position, Quaternion.identity);
         Vector3 dir = new Vector3(1, 2, 3);
         _rb.AddForce(dir * 2f, ForceMode.Impulse);
         _eventBus.Invoke(new DestroyTank());
--- Assets/Scripts/Weapon/Tank/TankShoot.cs
+++ Assets/Scripts/Weapon/Tank/TankShoot.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class TankShoot : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _effect;
     [SerializeField] private GameObject _bullet;
     [SerializeField] private Transform _spawnBullet;
     [SerializeField] private float _shootForce = 100;
@@ -33,6 +34,7 @@ public class TankShoot : MonoBehaviour
 
         if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
+            Instantiate(_effect, _spawnBullet.position, Quaternion.identity);
             targetPoint = hit.point;
             Vector3 dirWithoutSpread = targetPoint - _spawnBullet.position;
             GameObject currentBullet = Instantiate(_bullet, _spawnBullet.position, _spawnBullet.rotation);
