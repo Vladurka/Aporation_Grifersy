f76da77457486f4ca13101ecea3ac319574f8cee
Vladurka <0986535843v@gmail.com>
2024-07-11 18:22:51 +0200

    dwadaw

--- Assets/Scripts/PlayCutScene.cs
+++ Assets/Scripts/PlayCutScene.cs
@@ -1,18 +1,10 @@
 using UnityEngine;
-using UnityEngine.Playables;
 
 public class PlayCutScene : MonoBehaviour
 {
-    [SerializeField] private Camera _mainCamera;
-    [SerializeField] private Camera _cutCamera;
-    [SerializeField] private Camera _cameraUI;
-    [SerializeField] private Canvas _canvas;
-    public void PlayScene(PlayableDirector _cutScene)
+    [SerializeField] private GameObject _videoPanel;
+    public void CutScenePlay()
     {
-        _mainCamera.enabled = false;
-        _cutCamera.enabled = true;
-        _cameraUI.enabled = false;
-        _canvas.enabled = false;
-        _cutScene.Play();
+        _videoPanel.SetActive(true);
     }
 }
