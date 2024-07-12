a2b664f95f30a409a410701b7bcc288760576b7b
Vladurka <0986535843v@gmail.com>
2024-07-12 08:56:24 +0200

    dawd

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+    [SerializeField] private Text _fpsText;  
+
+    private int _frameCount = 0;
+    private float _deltaTime = 0.0f;
+    private float _fps = 0.0f;
+    private float _updateInterval = 1.0f; 
+
+    void Update()
+    {
+        _frameCount++;
+        _deltaTime += Time.deltaTime;
+
+        if (_deltaTime > _updateInterval)
+        {
+            _fps = _frameCount / _deltaTime;
+            _fpsText.text = _fps.ToString();
+
+            _frameCount = 0;
+            _deltaTime -= _updateInterval;
+        }
+    }
+}
--- Assets/Scripts/GameSystems/MissionsController.cs
+++ Assets/Scripts/GameSystems/MissionsController.cs
@@ -7,18 +7,22 @@ public class MissionsController : MonoBehaviour
     [SerializeField] private GameObject[] _missionsButtons;
 
     [SerializeField] private GameObject _car;
+
     [SerializeField] private GameObject _mission3Enemies;
+    [SerializeField] private GameObject _mission4Enemies;
 
     public void Init()
     {
         _car.SetActive(false);
+        _mission3Enemies.SetActive(false);
+        _mission4Enemies.SetActive(false);
 
         foreach (GameObject button in _missionsButtons)
         {
             button.SetActive(false);
         }
 
-        //MissionCondition = PlayerPrefs.GetInt(ConstSystem.MISSION_KEY);
+        MissionCondition = PlayerPrefs.GetInt(ConstSystem.MISSION_KEY);
 
         if (MissionCondition == 1)
         {
@@ -48,9 +52,22 @@ public class MissionsController : MonoBehaviour
 
 
         if (MissionCondition >= 3)
-        {
             _car.SetActive(true);
+
+    }
+
+    public void CurrentMission(int currentIndex)
+    {
+        if(currentIndex == 3)
+            _mission3Enemies.SetActive(true);
+
+        if(currentIndex == 4)
+            _mission4Enemies.SetActive(true);
+
+        if(currentIndex != 3)
             _mission3Enemies.SetActive(false);
-        }
+
+        if (currentIndex != 4)
+            _mission4Enemies.SetActive(false);
     }
 }
