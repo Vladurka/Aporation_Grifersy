7fd493c9e4636b763e8e3c4f5531fa887920ec02
Vladurka <0986535843v@gmail.com>
2024-07-22 18:21:51 +0200

    gre

--- Assets/Scripts/Plane/PlaneController.cs
+++ Assets/Scripts/Plane/PlaneController.cs
@@ -2,17 +2,26 @@ using UnityEngine;
 public class PlaneController : MonoBehaviour 
 {
     [SerializeField] private float _flySpeed;
-    [SerializeField] private float _maxSpeed = 50f;
-    [SerializeField] private float _minSpeed = 1f;
-    [SerializeField] private float _force = 0.01f;
+    [SerializeField] private float _maxSpeed = 110f;
+    [SerializeField] private float _minSpeed = 35f;
+
+    [SerializeField] private float _force = 0.08f;
+
+    [SerializeField] private float _pitchAngle = 70f;
+    [SerializeField] private float _rollAngle = 45f;
+
     private float _pitchSmoothness;
     private float _rollSmoothness;
 
+    private float _rollSpeed;
+    private float _pitchSpeed;
+
     private float horizontalMovement;
     private float Amount = 120;
+
     private Rigidbody rb;
 
-    private void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         _flySpeed = 0f;
@@ -21,11 +30,11 @@ public class PlaneController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.F) && _flySpeed < _maxSpeed)
+        if (Input.GetKey(KeyCode.Space) && _flySpeed < _maxSpeed)
             _flySpeed += _force;
 
 
-        if (Input.GetKey(KeyCode.G) && _flySpeed > 0)
+        if (Input.GetKey(KeyCode.LeftShift) && _flySpeed > _minSpeed + 1f)
             _flySpeed -= _force;
 
         if (_flySpeed >= _minSpeed)
@@ -34,13 +43,23 @@ public class PlaneController : MonoBehaviour
             Movement();
         }
 
-        if(_flySpeed <= _minSpeed)
-            rb.useGravity = true;
-
         transform.Translate(Vector3.forward * _flySpeed * Time.deltaTime);
 
-        _pitchSmoothness = _flySpeed / 20;
-        _rollSmoothness = _flySpeed / 20;
+        if (_flySpeed <= 70f)
+        {
+            _pitchSmoothness = _flySpeed / 20f;
+            _rollSmoothness = _flySpeed / 20f;
+            _rollSpeed = _rollSmoothness / 10f;
+            _pitchSpeed = _pitchSmoothness / 20f;
+        }
+
+        else if (_flySpeed > 70f)
+        {
+            _pitchSmoothness = 3.5f;
+            _rollSmoothness = 3.5f;
+            _rollSpeed = 0.35f;
+            _pitchSpeed = 0.175f;
+        }
     }
 
     private void Movement()
@@ -48,12 +67,12 @@ public class PlaneController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        horizontalMovement += horizontal * Amount * Time.deltaTime;
+        horizontalMovement += horizontal * Amount * _rollSpeed * Time.deltaTime;
 
-        float targetVerticalMovement = Mathf.LerpAngle(0, 45, Mathf.Abs(vertical)) * Mathf.Sign(vertical);
-        float verticalMovement = Mathf.LerpAngle(transform.localRotation.eulerAngles.x, targetVerticalMovement, Time.deltaTime * _pitchSmoothness);
+        float targetVerticalMovement = Mathf.LerpAngle(0, _pitchAngle, Mathf.Abs(vertical)) * Mathf.Sign(vertical);
+        float verticalMovement = Mathf.LerpAngle(transform.localRotation.eulerAngles.x, targetVerticalMovement, Time.deltaTime * _pitchSmoothness * _pitchSpeed);
 
-        float targetRoll = Mathf.LerpAngle(0, 45, Mathf.Abs(horizontal)) * -Mathf.Sign(horizontal);
+        float targetRoll = Mathf.LerpAngle(0, _rollAngle, Mathf.Abs(horizontal)) * -Mathf.Sign(horizontal);
         float roll = Mathf.LerpAngle(transform.localRotation.eulerAngles.z, targetRoll, Time.deltaTime * _rollSmoothness);
 
         transform.localRotation = Quaternion.Euler(verticalMovement, horizontalMovement, roll);
---
+++ Assets/Scripts/Plane/TransportCamera.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class TransportCamera : MonoBehaviour
+{
+    [SerializeField ] private Transform _target;
+    [SerializeField] private float _distance = 10.0f;
+    [SerializeField] private float _xSpeed = 120.0f;
+    [SerializeField] private float _ySpeed = 120.0f;
+
+    [SerializeField] private float _yMinLimit = -20f;
+    [SerializeField] private float _yMaxLimit = 80f; 
+
+    private float _x = 0.0f;
+    private float _y = 0.0f;
+
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        _x = angles.y;
+        _y = angles.x;
+
+        if (GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        if (_target)
+        {
+            _x += Input.GetAxis("Mouse X") * _xSpeed * 0.02f;
+            _y -= Input.GetAxis("Mouse Y") * _ySpeed * 0.02f;
+
+            _y = ClampAngle(_y, _yMinLimit, _yMaxLimit);
+
+            Quaternion rotation = Quaternion.Euler(_y, _x, 0);
+            Vector3 position = rotation * new Vector3(0.0f, 0.0f, -_distance) + _target.position;
+
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+    }
+
+    private static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
