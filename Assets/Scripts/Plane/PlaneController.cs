41209d150236475f3a8bf47270600df595f5ca54
Vladurka <0986535843v@gmail.com>
2024-07-23 11:34:21 +0200

    вцфф

--- Assets/Scripts/Plane/PlaneController.cs
+++ Assets/Scripts/Plane/PlaneController.cs
@@ -2,13 +2,26 @@ using UnityEngine;
 public class PlaneController : MonoBehaviour 
 {
     [SerializeField] private float _flySpeed;
-    [SerializeField] private float _maxSpeed = 110f;
-    [SerializeField] private float _minSpeed = 35f;
+    [SerializeField] private float _maxSpeed = 120f;
+    [SerializeField] private float _minSpeed = 70f;
+    [SerializeField] private float _startSpeed = 50f;
+
+    [SerializeField] private Transform _centerOfMass;
 
     [SerializeField] private float _force = 0.08f;
 
     [SerializeField] private float _pitchAngle = 70f;
     [SerializeField] private float _rollAngle = 45f;
+
+    [SerializeField] private Transform _leftFlap;
+    [SerializeField] private Transform _rightFlap;
+
+    [SerializeField] private Transform _leftWing;
+    [SerializeField] private Transform _rightWing;
+    [SerializeField] private float _wingSpeed = 2f;
+
+    [SerializeField] private float _flapSpeed = 15f;
+    [SerializeField] private float _flapAngle = 20f;
 
     private float _pitchSmoothness;
     private float _rollSmoothness;
@@ -16,35 +29,68 @@ public class PlaneController : MonoBehaviour
     private float _rollSpeed;
     private float _pitchSpeed;
 
-    private float horizontalMovement;
-    private float Amount = 120;
+    private float _horizontalMovement;
+    private float _amount = 120;
+
+    private bool _isClosed = false;
 
     private Rigidbody rb;
+    private Animator _animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
         _flySpeed = 0f;
         rb.useGravity = true;
     }
 
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.Space) && _flySpeed < _maxSpeed)
-            _flySpeed += _force;
-
-
-        if (Input.GetKey(KeyCode.LeftShift) && _flySpeed > _minSpeed + 1f)
-            _flySpeed -= _force;
-
-        if (_flySpeed >= _minSpeed)
+        if (_flySpeed >= _startSpeed)
         {
             rb.useGravity = false;
             Movement();
         }
 
+        if (Input.GetKey(KeyCode.Space) && _flySpeed < _maxSpeed)
+            _flySpeed += _force;
+
+
+        if (Input.GetKey(KeyCode.Space) && _flySpeed > 80)
+        {
+            if(_leftWing.localRotation.y >= -45 && _rightWing.localRotation.y <= 45)
+            {
+                Quaternion leftWingTargetRotation = Quaternion.Euler(-180, -45, 0);
+                Quaternion rightWingTargetRotation = Quaternion.Euler(0, 45, 0);
+
+                _leftWing.localRotation = Quaternion.Lerp(_leftWing.localRotation, leftWingTargetRotation, Time.deltaTime * _wingSpeed);
+                _rightWing.localRotation = Quaternion.Lerp(_rightWing.localRotation, rightWingTargetRotation, Time.deltaTime * _wingSpeed);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (_leftWing.localRotation.y <= -12 && _rightWing.localRotation.y >= 12)
+            {
+                Quaternion leftWingTargetRotation = Quaternion.Euler(-180, -12, 0);
+                Quaternion rightWingTargetRotation = Quaternion.Euler(0, 12, 0);
+
+                _leftWing.localRotation = Quaternion.Lerp(_leftWing.localRotation, leftWingTargetRotation, Time.deltaTime * _wingSpeed);
+                _rightWing.localRotation = Quaternion.Lerp(_rightWing.localRotation, rightWingTargetRotation, Time.deltaTime * _wingSpeed);
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftShift) && _flySpeed > _minSpeed + 1f)
+            _flySpeed -= _force;
+
         transform.Translate(Vector3.forward * _flySpeed * Time.deltaTime);
 
+    }
+
+    private void Update()
+    {
         if (_flySpeed <= 70f)
         {
             _pitchSmoothness = _flySpeed / 20f;
@@ -60,6 +106,39 @@ public class PlaneController : MonoBehaviour
             _rollSpeed = 0.35f;
             _pitchSpeed = 0.175f;
         }
+
+        if (_flySpeed >= 60f && !_isClosed)
+        {
+            _animator.SetTrigger("CloseWheels");
+            _isClosed = true;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Quaternion leftFlapTargetRotation = Quaternion.Euler(-_flapAngle, 0, 0);
+            Quaternion rightFlapTargetRotation = Quaternion.Euler(-200, 0, 0);
+
+            _leftFlap.localRotation = Quaternion.Lerp(_leftFlap.localRotation, leftFlapTargetRotation, Time.deltaTime * _flapSpeed);
+            _rightFlap.localRotation = Quaternion.Lerp(_rightFlap.localRotation, rightFlapTargetRotation, Time.deltaTime * _flapSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Quaternion leftFlapTargetRotation = Quaternion.Euler(_flapAngle, 0, 0);
+            Quaternion rightFlapTargetRotation = Quaternion.Euler(-160, 0, 0);
+
+            _leftFlap.localRotation = Quaternion.Lerp(_leftFlap.localRotation, leftFlapTargetRotation, Time.deltaTime * _flapSpeed);
+            _rightFlap.localRotation = Quaternion.Lerp(_rightFlap.localRotation, rightFlapTargetRotation, Time.deltaTime * _flapSpeed);
+        }
+
+        if (!Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
+        {
+            Quaternion leftFlapTargetRotation = Quaternion.Euler(0, 0, 0);
+            Quaternion  rightFlapTargetRotation = Quaternion.Euler(-180, 0, 0);
+
+            _leftFlap.localRotation = Quaternion.Lerp(_leftFlap.localRotation, leftFlapTargetRotation, Time.deltaTime * _flapSpeed);
+            _rightFlap.localRotation = Quaternion.Lerp(_rightFlap.localRotation, rightFlapTargetRotation, Time.deltaTime * _flapSpeed);
+        }
     }
 
     private void Movement()
@@ -67,7 +146,7 @@ public class PlaneController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        horizontalMovement += horizontal * Amount * _rollSpeed * Time.deltaTime;
+        _horizontalMovement += horizontal * _amount * _rollSpeed * Time.deltaTime;
 
         float targetVerticalMovement = Mathf.LerpAngle(0, _pitchAngle, Mathf.Abs(vertical)) * Mathf.Sign(vertical);
         float verticalMovement = Mathf.LerpAngle(transform.localRotation.eulerAngles.x, targetVerticalMovement, Time.deltaTime * _pitchSmoothness * _pitchSpeed);
@@ -75,6 +154,6 @@ public class PlaneController : MonoBehaviour
         float targetRoll = Mathf.LerpAngle(0, _rollAngle, Mathf.Abs(horizontal)) * -Mathf.Sign(horizontal);
         float roll = Mathf.LerpAngle(transform.localRotation.eulerAngles.z, targetRoll, Time.deltaTime * _rollSmoothness);
 
-        transform.localRotation = Quaternion.Euler(verticalMovement, horizontalMovement, roll);
+        transform.localRotation = Quaternion.Euler(verticalMovement, _horizontalMovement, roll);
     }
 }
