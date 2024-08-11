6fb1f6f548de10914b62c10911be426ac65b5c64
Vladurka <0986535843v@gmail.com>
2024-08-11 22:24:13 +0200

    CutScene + asyncLoading

--- Assets/Scripts/CutScenes/PlayCutScene.cs
+++ Assets/Scripts/CutScenes/PlayCutScene.cs
@@ -1,24 +1,34 @@
 using UnityEngine;
-using UnityEngine.Playables;
+using UnityEngine.Video;
 
 public class PlayCutScene : MonoBehaviour
 {
-    [SerializeField] private PlayableDirector _playableDirector;
+    [SerializeField] private VideoPlayer _video;
+    [SerializeField] private GameObject[] _panels;
+    [SerializeField] private GameObject _videoPanel;
     private Loading _loading;
 
     private void Start()
     {
         _loading = ServiceLocator.Current.Get<Loading>();
+        Debug.Log(_loading);
     }
 
-    public void PlayCutscene(PlayableDirector playableDirector)
+    public void PlayVideo(int index)
     {
-        //_playableDirector.stopped += (director) => OnPlayableDirectorStopped(index);
-        playableDirector.Play();
+        Cursor.lockState = CursorLockMode.Locked;
+        ConstSystem.CanPause = false;
+
+        _videoPanel.SetActive(true);
+        _video.Play();
+        _video.loopPointReached += (vp) => Load(vp, index);
     }
 
-    private void OnPlayableDirectorStopped(int index)
+    private void Load(VideoPlayer vp, int index)
     {
+        foreach (GameObject panel in _panels)
+            panel.SetActive(false);
+
         _loading.StartLoading(index);
     }
 }
--- Assets/Scripts/GameSystems/ConstSystem.cs
+++ Assets/Scripts/GameSystems/ConstSystem.cs
@@ -44,6 +44,7 @@ public static class ConstSystem
     public const string MISSION_KEY = "MISSION";
 
     public static bool CanSave;
+    public static bool CanPause = true;
     public static bool InTransport = false;
     public static bool InCar = false;
     public static bool CanExit = true;
