ef0ff44d583c2bc29c8b3ba2eb43b4710f9d2f82
Vladurka <0986535843v@gmail.com>
2024-10-10 18:03:53 +0200

    SkinsFix

--- Assets/Scripts/Shop/Purchaser.cs
+++ Assets/Scripts/Shop/Purchaser.cs
@@ -4,41 +4,36 @@ public class Purchaser : MonoBehaviour
 {
     public void BuyNewAkSkin(SkinData info)
     {
-        PlayerPrefs.SetInt(ConstSystem.AK_SKIN_INDEX, info.Number);
-        PlayerPrefs.SetInt(info.Key, 1);
-        PlayerPrefs.Save();
+        PlayerPrefsSafe.SetInt(ConstSystem.AK_SKIN_INDEX, info.Number);
+        PlayerPrefsSafe.SetInt(info.Key, 1);
         info.UseButton.SetActive(true);
     }
 
     public void BuyNewKnifeSkin(SkinData info)
     {
-        PlayerPrefs.SetInt(ConstSystem.KNIFE_SKIN_INDEX, info.Number);
-        PlayerPrefs.SetInt(info.Key, 1);
-        PlayerPrefs.Save();
+        PlayerPrefsSafe.SetInt(ConstSystem.KNIFE_SKIN_INDEX, info.Number);
+        PlayerPrefsSafe.SetInt(info.Key, 1);
         info.UseButton.SetActive(true);
     }
 
     public void BuyNewCarSkin(SkinData info)
     {
-        PlayerPrefs.SetInt(ConstSystem.CAR_SKIN_INDEX, info.Number);
-        PlayerPrefs.SetInt(info.Key, 1);
-        PlayerPrefs.Save();
+        PlayerPrefsSafe.SetInt(ConstSystem.CAR_SKIN_INDEX, info.Number);
+        PlayerPrefsSafe.SetInt(info.Key, 1);
         info.UseButton.SetActive(true);
     }
 
     public void BuyNewRpgSkin(SkinData info)
     {
-        PlayerPrefs.SetInt(ConstSystem.RPG_SKIN_INDEX, info.Number);
-        PlayerPrefs.SetInt(info.Key, 1);
-        PlayerPrefs.Save();
+        PlayerPrefsSafe.SetInt(ConstSystem.RPG_SKIN_INDEX, info.Number);
+        PlayerPrefsSafe.SetInt(info.Key, 1);
         info.UseButton.SetActive(true);
     }
 
     public void BuyNewGlovesSkin(SkinData info)
     {
-        PlayerPrefs.SetInt(ConstSystem.GLOVES_SKIN_INDEX, info.Number);
-        PlayerPrefs.SetInt(info.Key, 1);
-        PlayerPrefs.Save();
+        PlayerPrefsSafe.SetInt(ConstSystem.GLOVES_SKIN_INDEX, info.Number);
+        PlayerPrefsSafe.SetInt(info.Key, 1);
         info.UseButton.SetActive(true);
     }
 }
